31936f1b7437d5649eceac152dc01f9e547739f9
Amit <amit.mondol@bjitgroup.com>
2023-10-19 15:51:40 +0600

    Initial Commit

---
+++ Little Lemon dinner menu app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Little Lemon dinner menu app
+//
+//  Created by 11461_amit on 19/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Little Lemon dinner menu app/Little_Lemon_dinner_menu_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_dinner_menu_appApp.swift
+//  Little Lemon dinner menu app
+//
+//  Created by 11461_amit on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_dinner_menu_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
